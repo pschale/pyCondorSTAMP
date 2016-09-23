0d04f2051a21efc6654b6f0cd7e36b343bd68607
pschale <pschale@uoregon.edu>
2016-09-23 15:03:00 -0700

    removed a couple old params

--- pyCondorSTAMPanteproc_full.py
+++ pyCondorSTAMPanteproc_full.py
@@ -635,20 +635,20 @@ def main():
     # find frame files
     for tempJob in set(H1_jobs):
         print("Finding frames for job " + str(tempJob) + " for H1")
-        tempJobData = jobDataDict[str(tempJob)]
+        #tempJobData = jobDataDict[str(tempJob)]
         if input_params["simulated"] == "false":
             temp_frames = create_frame_file_list("H1_" + input_params['frame_type'], times[H1_job_index][1] - 2, times[H1_job_index][1] + 1602, "H")
             create_cache_and_time_file(temp_frames, "H",tempJob,cacheDir, archived_frames_okay = archived_frames_okay)
         else:
-            create_fake_cache_and_time_file(tempJobData[0], tempJobData[1], "H", tempJob, fakeCacheDir)
+            create_fake_cache_and_time_file(times[H1_job_index][1] - 2, times[H1_job_index][1] + 1602, "H", tempJob, fakeCacheDir)
     for tempJob in set(L1_jobs):
         print("Finding frames for job " + str(tempJob) + " for L1")
-        tempJobData = jobDataDict[str(tempJob)]
+        #tempJobData = jobDataDict[str(tempJob)]
         if input_params["simulated"] == "false":
             temp_frames = create_frame_file_list("L1_" + input_params['frame_type'], times[L1_job_index][1] - 2, times[L1_job_index][1] + 1602, "L")
             create_cache_and_time_file(temp_frames, "L",tempJob,cacheDir, archived_frames_okay = archived_frames_okay)
         else:
-            create_fake_cache_and_time_file(tempJobData[0], tempJobData[1], "L", tempJob, fakeCacheDir)
+            create_fake_cache_and_time_file(times[H1_job_index][1] - 2, times[H1_job_index][1] + 1602, "L", tempJob, fakeCacheDir)
             
     for job in stochtrackParamsList:
         job['grandStochtrackParams'] = recursive_ints_to_floats(job['grandStochtrackParams'])
