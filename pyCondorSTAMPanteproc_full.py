21c9d0f4990d8446311da22b86b7a9f5736fff71
pschale <pschale@uoregon.edu>
2016-09-26 17:03:09 -0700

    correctly named input_params['job_start_shift']

--- pyCondorSTAMPanteproc_full.py
+++ pyCondorSTAMPanteproc_full.py
@@ -765,7 +765,7 @@ def main():
     newAdjustedJobPath = glueFileLocation(supportDir, jobPath[len(jobPath)-jobPath[::-1].index('/')::])
     with open(input_params['jobFile']) as h:
         jobData = [[int(x) for x in line.split()] for line in h]
-    adjustedJobData = [[x[0], x[1] + input_params['start_shift'], x[1] + input_params['start_shift'] + input_params['duration'], input_params['duration']] for x in jobData]
+    adjustedJobData = [[x[0], x[1] + input_params['job_start_shift'], x[1] + input_params['job_start_shift'] + input_params['duration'], input_params['duration']] for x in jobData]
     adjustedJobText = "\n".join(" ".join(str(x) for x in line) for line in adjustedJobData)
     with open(newAdjustedJobPath) as h:   
         print >> h, adjustedJobText 
