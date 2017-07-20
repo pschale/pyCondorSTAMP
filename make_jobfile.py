4d85e436af97ddcfa21ed16979fe8364724bb1f7
Paul Schale <paul.schale@ligo.org>
2017-07-20 10:40:51 -0700

    fixed bug in job file creation (sometimes non-observing time was put in job files)

--- make_jobfile.py
+++ make_jobfile.py
@@ -34,11 +34,30 @@ elif options.frameType == 'C01':
 else:
     raise ValueError('Can only accept C00 or C01 for frametype')
 
+print('H1:' + segName)
+print(startTime)
+print(endTime)
+
 H1flag = DataQualityFlag.query('H1:' + segName, startTime, endTime).active
 L1flag = DataQualityFlag.query('L1:' + segName, startTime, endTime).active
 
+#print H1flag
+#print L1flag
+bothActive = H1flag & L1flag
 
-bothActive = H1flag and L1flag
+#verify that intersection was done correctly
+for seg in bothActive:
+
+    pass_H_test = Segment(seg) in H1flag
+    pass_L_test = Segment(seg) in L1flag
+
+    if not (pass_H_test and pass_L_test):
+        print(seg)
+        print('H in observing:', pass_H_test)
+        print('L in observing:', pass_L_test)
+        raise ValueError('Some segments not in observing time')
+
+#print bothActive
 
 #check if onsource has flag active
 if Segment(options.triggerTime - 100, options.triggerTime + 1700) not in bothActive:
@@ -71,6 +90,17 @@ for i in range(len(bothActive)):
     for j in range(numJobsinSeg[i]):
         offsource.append([startOfJob, startOfJob + options.duration])
         startOfJob += options.duration
+
+#validation of offsource
+for job in offsource:
+    pass_H_test = Segment(job) in H1flag
+    pass_L_test = Segment(job) in L1flag
+
+    if not (pass_H_test and pass_L_test):
+        print(job)
+        print('H in observing:', pass_H_test)
+        print('L in observing:', pass_L_test)
+        raise ValueError('Some segments not in observing time')
 
 # Reduce number of jobs to numJobs; end up with same number before and after - or as close as possible
 numBeforeTrigger = len([ele for ele in offsource if ele[0] < options.triggerTime])
